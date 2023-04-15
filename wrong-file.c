fabb982630efed47fb8c46da7a05dcee71ff101f
golnazgh <golnazgh@users.noreply.github.com>
2023-04-15 10:07:24 -0700

    Update wrong-file.c

--- wrong-file.c
+++ wrong-file.c
@@ -18,12 +18,6 @@ static char
 (safe_add_func_int8_t_s_s)(char si1, char si2 )
 {
  
-  return
-
-
-    (((si1>0) && (si2>0) && (si1 > ((127)-si2))) || ((si1<0) && (si2<0) && (si1 < ((-128)-si2)))) ?
-    ((si1)) :
-
-
-    (si1 + si2);
+  return (((si1>0) && (si2>0) && (si1 > ((127)-si2))) || ((si1<0) && (si2<0) && (si1 < ((-128)-si2)))) ?
+    ((si1)) : (si1 + si2);
 }
